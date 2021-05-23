60b3b268373ea3b9c931f5698f9a3ef026253fe0
francescoalterio <francescoalteriog@gmail.com>
2021-05-23 14:12:45 -0400

    lectura de database y aparicion de techs

---
+++ js/tecnologies.js
@@ -0,0 +1,20 @@
+function cargarTecnologias () {
+    const containerTecnologias = document.querySelector('.container-tecnologias')
+
+    const tecnologias = [...database];
+
+
+    tecnologias.forEach(element => {
+        const tecnologia = document.createElement('div');
+        tecnologia.classList.add('tecnologia');
+        tecnologia.innerHTML = `<img class="tech-img" src="assets/HTML5_Badge_512.png" alt="" srcset="">
+        <p class="title-tech fs-2 fw-bold">${element.nombre}</p>
+        <div class="box-tech-btn">
+            <button class="btn-add-list btn btn-primary">Add to list</button>
+            <button class="btn-learn btn btn-success">Learn</button>
+        </div>`
+
+        containerTecnologias.appendChild(tecnologia)
+    });
+}
+
